6f0b47118351a093337c2bc11fe942cca6f27f22
ykhs <ykhs.jp@gmail.com>
2011-09-20 06:21:51 +0900

    rewrite and make document

--- document/js/sample.js
+++ document/js/sample.js
@@ -1,152 +1,116 @@
+$(function () {
 
-(function () {
-    $(function () {
+    /*
+     * Get the virtual position of the dragged element
+     */
+    (function () {
 
-        var info_fx = $('#info1-drag-fx'),
-            info_x = $('#info1-drag-x'),
-            info_dx = $('#info1-drag-dx'),
-            info_sx = $('#info1-drag-sx'),
-            info_fy = $('#info1-drag-fy'),
-            info_y = $('#info1-drag-y'),
-            info_dy = $('#info1-drag-dy'),
-            info_sy = $('#info1-drag-sy'),
-            $drag1 = $('#drag1');
+        var $status = $('#info-status'),
+            $startX = $('#info-start-x'),
+            $x = $('#info-x'),
+            $destinationX = $('#info-destination-x'),
+            $speedX = $('#info-speed-x'),
+            $startY = $('#info-start-y'),
+            $y = $('#info-y'),
+            $destinationY = $('#info-destination-y'),
+            $speedY = $('#info-speed-y'),
+            $obj = $('#info-drag-object');
 
-        $drag1
-        .phantomDrag({
+        $obj.phantomDrag({
+            start: function (e, res) {
+                $status.val(e.type.replace(/phantomdrag/, ''));
+            },
             step: function (e, res) {
-                console.debug(this, arguments);
-                info_fx.val(res.startX);
-                info_fy.val(res.startY);
-                info_x.val(res.currentX);
-                info_y.val(res.currentY);
-                info_dx.val(res.destinationX);
-                info_dy.val(res.destinationY);
+                $startX.val(res.startX);
+                $startY.val(res.startY);
+                $x.val(res.currentX);
+                $y.val(res.currentY);
+                $speedX.val(res.speedX);
+                $speedY.val(res.speedY);
+                $destinationX.val(res.destinationX);
+                $destinationY.val(res.destinationY);
+            },
+            release: function (e, res) {
+                $status.val(e.type.replace(/phantomdrag/, ''));
+            },
+            stop: function (e, res) {
+                $status.val(e.type.replace(/phantomdrag/, ''));
             }
         })
-    });
-})();
+    })();
 
+    /*
+     * Move the dragged element
+     */
+    (function () {
 
-(function () {
-    $(function () {
+        var $elements = $('.drag2');
 
-        var elements = $('.drag2');
-
-        elements
-        .phantomDrag({
-            xmax: 660,
-            xmin: 0,
-            ymax: 260,
-            ymin: 0,
-            delay: 13
-        })
-        .bind('phantomdrag-start', function (e) {
-            elements.css({
-                background: '#559bf1'
-            });
-            $(e.target).css({
-                background: '#f2a649'
-            })
-        })
-        .bind('phantomdrag-move', function (e) {
-            $(e.target).css({
-                top: $(e.target).data('phantomdragY'),
-                left: $(e.target).data('phantomdragX')
-            })
-        })
-        .bind('phantomdrag-release', function (e) {
-            $(e.target).css({
-                background: '#74e87a'
-            })
-        })
-        .bind('phantomdrag-stop', function (e) {
-            $(e.target).css({
-                background: '#559bf1'
-            })
+        $elements.phantomDrag({
+            xMax: 860,
+            xMin: 0,
+            yMax: 300,
+            yMin: 0,
+            step: function (e, res) {
+                $(e.target).css({
+                    top: res.currentY,
+                    left: res.currentX
+                })
+            }
         });
-    });
-})();
+    })();
 
-(function () {
-    $(function () {
 
-        var element = $('#drag3'),
-            mover = $('#mover1');
+    /*
+     * Move to another element
+     */
+    (function () {
 
-        element
-        .phantomDrag({
-            xmax: 71,
-            xmin: 0,
-            ymax: 31,
-            ymin: 0,
-            stopSpeedBorder: 0.01
-        })
-        .bind('phantomdrag-start', function (e) {
-            mover.css({
-                background: '#f2a649'
-            });
-        })
-        .bind('phantomdrag-move', function (e) {
-            mover.css({
-                top: element.data('phantomdragY') * 10,
-                left: element.data('phantomdragX') * 10
-            })
-        })
-        .bind('phantomdrag-release', function (e) {
-            mover.css({
-                background: '#74e87a'
-            })
-        })
-        .bind('phantomdrag-stop', function (e) {
-            mover.css({
-                background: '#559bf1'
-            })
+        var $element = $('#drag3'),
+            $mover = $('#mover1');
+
+        $element.phantomDrag({
+            xMax: 910,
+            xMin: 0,
+            yMax: 350,
+            yMin: 0,
+            step: function (e, res) {
+                $mover.css({
+                    top: res.currentY,
+                    left: res.currentX
+                })
+            }
         });
-    });
+    })();
 
-})();
+    /*
+     * Converted to circular motion
+     */
+    (function () {
 
-(function () {
-    $(function () {
-
-        var element = $('#drag4'),
-            mover = $('#mover2'),
+        var $element = $('#drag4'),
+            $mover = $('#mover2'),
             deg = 0,
             r = 100,
             centerX = 455,
             centerY = 155,
             radian = null;
 
-        element
-        .phantomDrag({
-            delay: 13
-        })
-        .bind('phantomdrag-start', function (e) {
-            mover.css({
-                background: '#f2a649'
-            });
-        })
-        .bind('phantomdrag-move', function (e) {
+        $element.phantomDrag({
+            step: function (e, res) {
 
-            radian = Math.PI / 180 * deg;
+                radian = Math.PI / 180 * deg;
 
-            mover.css({
-                top: centerY + r * Math.cos(radian),
-                left: centerX + r * Math.sin(radian)
-            })
+                $mover.css({
+                    top: centerY + r * Math.cos(radian),
+                    left: centerX + r * Math.sin(radian)
+                })
 
-            deg += element.data('phantomdragSx');
+                deg += res.speedX;
+            }
         })
-        .bind('phantomdrag-release', function (e) {
-            mover.css({
-                background: '#74e87a'
-            })
-        })
-        .bind('phantomdrag-stop', function (e) {
-            mover.css({
-                background: '#559bf1'
-            })
-        });
-    });
-})();
+    })();
+
+
+    prettyPrint();
+});
--- jquery.ui.phantomDrag.js
+++ jquery.ui.phantomDrag.js
@@ -8,8 +8,6 @@
 * http://jquery.org/license
 */
 
-/**
-*/
 (function ($, window, document, undefined) {
 
     $.widget('ui.phantomDrag', {
@@ -17,11 +15,11 @@
         options: {
             defaultX: 0,
             defaultY: 0,
-            xMax: Number.POSITIVE_INFINITY,
-            xMin: Number.NEGATIVE_INFINITY,
-            yMax: Number.POSITIVE_INFINITY,
-            yMin: Number.NEGATIVE_INFINITY,
-            degree: 13
+            xMax: Infinity,
+            xMin: -Infinity,
+            yMax: Infinity,
+            yMin: -Infinity,
+            delay: 13
         },
 
         destinationX: 0,
@@ -40,133 +38,138 @@
 
         _init: function () {
 
-            var self = this,
+            var that = this,
                 o = this.options;
 
-            self.destinationX = self.currentX = o.defaultX;
-            self.destinationY = self.currentY = o.defaultY;
+            that.destinationX = that.currentX = o.defaultX;
+            that.destinationY = that.currentY = o.defaultY;
 
-            self.element.mousedown($.proxy(self._dragStart, self));
-            return self;
-        },
-
-        moveTo: function (x, y) {
-
-            var self = this,
-                o = self.options;
-
-            self.destinationX = Math.max(o.xMin, Math.min(x, o.xMax));
-            self.destinationY = Math.max(o.yMin, Math.min(y, o.yMax));
-
-            this._startAnimationTimer();
-        },
-
-        _startAnimationTimer: function () {
-
-            var self = this;
-
-            self._stopTimer();
-            self.intervalID = setInterval($.proxy(self._step, self), self.interval);
-        },
-
-        _stopTimer: function () {
-
-            var self = this;
-
-            clearInterval(self.intervalID);
-            self.intervalID = 0;
-        },
-
-        _step: function () {
-
-            var self = this,
-                o = self.options,
-                distanceX = self.destinationX - self.currentX,
-                distanceY = self.destinationY - self.currentY,
-                distance = Math.sqrt((distanceX * distanceX) + (distanceY * distanceY));
-
-            if (distance > 1 || self.mousedown) {
-                self.speedX = distanceX / self.options.degree;
-                self.speedY = distanceY / self.options.degree;
-                self.currentX += self.speedX;
-                self.currentY += self.speedY;
-            } else {
-                self.currentX = self.destinationX;
-                self.currentY = self.destinationY;
-                self._stopTimer();
-                self._trigger('stop', 0, {
-                    startX: self.startX,
-                    startY: self.startY,
-                    destinationX: self.destinationX,
-                    destinationY: self.destinationY,
-                    speedX: self.speedX,
-                    speedY: self.speedY,
-                    currentX: self.currentX,
-                    currentY: self.currentY
-                });
-            }
-
-            self._trigger('move', 0, {
-                startX: self.startX,
-                startY: self.startY,
-                destinationX: self.destinationX,
-                destinationY: self.destinationY,
-                speedX: self.speedX,
-                speedY: self.speedY,
-                currentX: self.currentX,
-                currentY: self.currentY
-            });
-            return false;
+            that.element.mousedown($.proxy(that._dragStart, that));
+            return that;
         },
 
         _dragStart: function (e) {
 
-            var self = this,
-                o = self.options;
+            var that = this,
+                o = that.options;
 
-            self._startAnimationTimer();
-            self.mousedown = true;
-            self.startX = self.currentX;
-            self.startY = self.currentY;
-            self.mouseStartX = e.pageX;
-            self.mouseStartY = e.pageY;
-            $document.bind('mousemove', $.proxy(self._drag, self));
-            $document.bind('mouseup', $.proxy(self._dragEnd, self));
-            self._trigger('start');
+            that._startLoopTimer();
+            that.mousedown = true;
+            that.startX = that.currentX;
+            that.startY = that.currentY;
+            that.mouseStartX = e.pageX;
+            that.mouseStartY = e.pageY;
+            $(document).bind({
+                mousemove: $.proxy(that._drag, that),
+                mouseup: $.proxy(that._dragEnd, that)
+            });
+            that._trigger('start');
             return false;
+        },
+
+        _startLoopTimer: function () {
+
+            var that = this;
+
+            that._stopTimer();
+            that.intervalID = setInterval($.proxy(that._step, that), that.interval);
+        },
+
+        _stopTimer: function () {
+
+            var that = this;
+
+            clearInterval(that.intervalID);
+            that.intervalID = 0;
         },
 
         _drag: function (e) {
 
-            var self = this,
-                o = self.options,
+            var that = this,
+                o = that.options,
                 pageX = e.pageX,
                 pageY = e.pageY;
 
-            self.destinationX = Math.max(o.xMin, Math.min(self.startX + pageX - self.mouseStartX, o.xMax)),
-            self.destinationY = Math.max(o.yMin, Math.min(self.startY + pageY - self.mouseStartY, o.yMax));
+            that.destinationX = Math.max(o.xMin, Math.min(that.startX + pageX - that.mouseStartX, o.xMax)),
+            that.destinationY = Math.max(o.yMin, Math.min(that.startY + pageY - that.mouseStartY, o.yMax));
 
+            return false;
+        },
+
+        _step: function () {
+
+            var that = this,
+                o = that.options,
+                distanceX = that.destinationX - that.currentX,
+                distanceY = that.destinationY - that.currentY,
+                distance = Math.sqrt((distanceX * distanceX) + (distanceY * distanceY));
+
+            if (distance > 1 || that.mousedown) {
+                that.speedX = distanceX / o.delay;
+                that.speedY = distanceY / o.delay;
+                that.currentX += that.speedX;
+                that.currentY += that.speedY;
+            } else {
+                that.currentX = that.destinationX;
+                that.currentY = that.destinationY;
+                that.speedX = that.speedY = 0;
+                that._stopTimer();
+                that._trigger('stop', 0, {
+                    startX: that.startX,
+                    startY: that.startY,
+                    destinationX: that.destinationX,
+                    destinationY: that.destinationY,
+                    speedX: that.speedX,
+                    speedY: that.speedY,
+                    currentX: that.currentX,
+                    currentY: that.currentY
+                });
+            }
+
+            that._trigger('step', 0, {
+                startX: that.startX,
+                startY: that.startY,
+                destinationX: that.destinationX,
+                destinationY: that.destinationY,
+                speedX: that.speedX,
+                speedY: that.speedY,
+                currentX: that.currentX,
+                currentY: that.currentY
+            });
             return false;
         },
 
         _dragEnd: function (e) {
 
-            var self = this;
+            var that = this;
 
-            self.mousedown = false;
-            $document.unbind('mousemove', $.proxy(self._drag, self));
-            $document.unbind('mouseup', $.proxy(self._dragEnd, self));
-            self._trigger('release', 0, {
-                startX: self.startX,
-                startY: self.startY,
-                destinationX: self.destinationX,
-                destinationY: self.destinationY,
-                speedX: self.speedX,
-                speedY: self.speedY,
-                currentX: self.currentX,
-                currentY: self.currentY
+            that.mousedown = false;
+            $(document).unbind({
+                mousemove: $.proxy(that._drag, that),
+                mouseup: $.proxy(that._dragEnd, that)
+            });
+            that._trigger('release', 0, {
+                startX: that.startX,
+                startY: that.startY,
+                destinationX: that.destinationX,
+                destinationY: that.destinationY,
+                speedX: that.speedX,
+                speedY: that.speedY,
+                currentX: that.currentX,
+                currentY: that.currentY
             });
             return false;
+        },
+
+        moveTo: function (x, y) {
+
+            var that = this,
+                o = that.options;
+
+            that.destinationX = Math.max(o.xMin, Math.min(x, o.xMax));
+            that.destinationY = Math.max(o.yMin, Math.min(y, o.yMax));
+
+            this._startLoopTimer();
         }
     });
 })(jQuery, this, this.document, Math);
